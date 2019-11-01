ff384efda19c0a341aed26d0e688bdfbfbb9cbb5
3d maya <3d@gm>
2019-11-01 21:42:29 +0200

    Add VisualStudio project files.\n Add PDCurses library - used as ncurses implementation for Win32

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+#ifdef _WIN32
+#define PDC_DLL_BUILD
+#include "curses.h"
+#else
 #include <ncurses.h>
-#include <iostream>
+#endif
 #include <array>
+#include <iostream>
 #include <list>
 #include "snake.h"
-using namespace std;
 
 const int FIELD_SIZE_X = 20;
 const int FIELD_SIZE_Y = 15;
@@ -12,244 +16,239 @@ const int SNAKE_INIT_SIZE = 6;
 typedef std::array<std::array<char, FIELD_SIZE_X>, FIELD_SIZE_Y> GameFieldArray;
 typedef std::list<SnakeSegment> Snake;
 
-void drawField(const GameFieldArray &field, const Snake &snake);
+void init(GameFieldArray &field, Snake &snake);
 void initField(GameFieldArray &field, Snake &snake);
 Snake initSnake();
-void init(GameFieldArray &field, Snake &snake);
-SnakeSegment getNextMove(SnakeSegment &head);
-bool moveSnake(GameFieldArray &field, Snake &snake);
-bool fieldCrash(const GameFieldArray &field, const SnakeSegment &head);
-bool selfCrash(const Snake &snake, const SnakeSegment &head);
 
-unsigned int random(unsigned int min, unsigned int max) {
-    unsigned int rnd = static_cast<unsigned int>(rand());
-    return rnd % (max - min) + min;
-}
+void drawField(GameFieldArray &field, Snake &snake);
+bool moveSnake(GameFieldArray &field, Snake &snake);
+SnakeSegment getNextMove(SnakeSegment &head);
 
 void addApple(GameFieldArray &field, const Snake &snake);
-bool appleFound(GameFieldArray &field, const SnakeSegment &head);
+
+unsigned int random(unsigned int min, unsigned int max) {
+	unsigned int rnd = static_cast<unsigned int>(rand());
+	return rnd % (max - min) + min;
+}
 
 int main()
 {
-    GameFieldArray gameField;
-    Snake snake;
+	GameFieldArray gameField;
+	Snake snake;
 
-    init(gameField, snake);
+	init(gameField, snake);
 
-    bool exit = false;
+	bool exit = false;
 
-    while (!exit) {
-        int ch = getch();
-        bool crash = false;
+	while (!exit) {
+		int ch = getch();
+		bool crash = false;
 
-        switch ( ch )
-        {
-        case ERR:
-            clear();
-            drawField(gameField, snake);
-            crash = moveSnake(gameField, snake);
-            break;
-        case KEY_F(2): //–í—ã—Ö–æ–¥–∏–º –∏–∑ –ø—Ä–æ–≥—Ä–∞–º–º—ã, –µ—Å–ª–∏ –±—ã–ª–∞ –Ω–∞–∂–∞—Ç–∞ F2
-        case 'q': //–ò–ª–∏ 'q'
-            exit = true;
-            break;
-        case KEY_UP:
-        {
-            SnakeSegment &head = *snake.begin();
-            head.dirX = DirectionX::NONE;
-            head.dirY = DirectionY::UP;
-            break;
-        }
-        case KEY_DOWN:
-        {
-            SnakeSegment &head = *snake.begin();
-            head.dirX = DirectionX::NONE;
-            head.dirY = DirectionY::DOWN;
-            break;
-        }
-        case KEY_LEFT:
-        {
-            SnakeSegment &head = *snake.begin();
-            head.dirX = DirectionX::LEFT;
-            head.dirY = DirectionY::NONE;
-            break;
-        }
-        case KEY_RIGHT:
-        {
-            SnakeSegment &head = *snake.begin();
-            head.dirX = DirectionX::RIGHT;
-            head.dirY = DirectionY::NONE;
-            break;
-        }
-        default:  //–ï—Å–ª–∏ –≤—Å—ë –Ω–æ—Ä–º–∞–ª—å–Ω–æ, –≤—ã–≤–æ–¥–∏–º –∫–æ–¥ –Ω–∞–∂–∞—Ç–æ–π –∫–ª–∞–≤–∏—à–∏
-            printw("Code of pressed key is %d\n", ch);
-            break;
-        }
+		switch (ch)
+		{
+		case ERR:
+			clear();
+			drawField(gameField, snake);
+			crash = moveSnake(gameField, snake);
+			break;
+		case 'q':
+			exit = true;
+			break;
+		case KEY_UP:
+		{
+			SnakeSegment &head = snake.front();
+			head.dirX = DirectionX::NONE;
+			head.dirY = DirectionY::UP;
+			break;
+		}
+		case KEY_DOWN:
+		{
+			SnakeSegment &head = snake.front();
+			head.dirX = DirectionX::NONE;
+			head.dirY = DirectionY::DOWN;
+			break;
+		}
+		case KEY_LEFT:
+		{
+			SnakeSegment &head = snake.front();
+			head.dirX = DirectionX::LEFT;
+			head.dirY = DirectionY::NONE;
+			break;
+		}
+		case KEY_RIGHT:
+		{
+			SnakeSegment &head = snake.front();
+			head.dirX = DirectionX::RIGHT;
+			head.dirY = DirectionY::NONE;
+			break;
+		}
+		default:
+			break;
+		}
 
-        if (crash)
-        {
-            exit = true;
-            mvprintw(FIELD_SIZE_Y + 2, 5, "Oh no! You've crashed! Game over");
-        }
+		if (crash)
+		{
+			exit = true;
+			mvprintw(FIELD_SIZE_Y + 2, 5, "Oh no! You've crashed! Game over");
+		}
 
-        refresh();
-    }
+		refresh();
+	}
 
-    mvprintw(FIELD_SIZE_Y + 3, 5, "Press any key...");
-    cbreak();
-    getch();
+	cbreak();
+	getch();
 
-    endwin();                    // –í—ã—Ö–æ–¥ –∏–∑ curses-—Ä–µ–∂–∏–º–∞. –û–±—è–∑–∞—Ç–µ–ª—å–Ω–∞—è –∫–æ–º–∞–Ω–¥–∞.
-    return 0;
-}
+	endwin();                    // ¬˚ıÓ‰ ËÁ curses-ÂÊËÏ‡. Œ·ˇÁ‡ÚÂÎ¸Ì‡ˇ ÍÓÏ‡Ì‰‡.
 
-void drawField(const GameFieldArray &field, const Snake &snake) {
-    // Draw field itself
-    for (GameFieldArray::size_type i = 0; i < FIELD_SIZE_Y; ++i) {
-        const char* fieldStr = field[i].data();
-        mvprintw(static_cast<int>(i), 0, fieldStr);
-    }
+	system("pause");
 
-    //Draw snake
-    for (auto &snakeSeg : snake) {
-        mvaddch(snakeSeg.y, snakeSeg.x, '*');
-    }
+	return 0;
 }
 
 void addApple(GameFieldArray &field, const Snake &snake)
 {
-    bool placeFound = false;
-    Point apple;
+	bool placeFound = false;
+	Point apple;
 
-    while (!placeFound) {
-        placeFound = true;
+	while (!placeFound)
+	{
+		placeFound = true;
+		apple = Point(random(2, FIELD_SIZE_X - 2), random(FIELD_SIZE_Y / 2, FIELD_SIZE_Y - 2));
 
-        apple = Point(random(2, FIELD_SIZE_X - 2), random(FIELD_SIZE_Y / 2, FIELD_SIZE_Y - 1));
+		for (auto &segm : snake)
+		{
+			if (segm == apple)
+			{
+				placeFound = false;
+				break;
+			}
+		}
+	}
 
-        for (auto &segm : snake) {
-            if (segm == apple)
-            {
-                placeFound = false;
-                break;
-            }
-        }
-    }
-
-    field[apple.y][apple.x] = '@';
-}
-
-void initField(GameFieldArray &field, Snake &snake)
-{
-    for (GameFieldArray::size_type i = 0; i < FIELD_SIZE_Y - 1; ++i) {
-        field[i][FIELD_SIZE_X - 1] = '\0';
-    }
-
-    for (GameFieldArray::size_type i = 0; i < FIELD_SIZE_X - 1; ++i) {
-        field[0][i] = '*';
-        field[FIELD_SIZE_Y - 1][i] = '*';
-    }
-
-    for (GameFieldArray::size_type i = 1; i < FIELD_SIZE_Y - 1; ++i) {
-        field[i][0] = '*';
-        field[i][FIELD_SIZE_X - 2] = '*';
-
-        for (GameFieldArray::size_type j  = 1; j < FIELD_SIZE_X - 2; ++j) {
-            field[i][j] = ' ';
-        }
-    }
-
-    addApple(field, snake);
-}
-
-Snake initSnake()
-{
-    Snake snake;
-
-    SnakeSegment snakeHead((FIELD_SIZE_X - SNAKE_INIT_SIZE) / 2, FIELD_SIZE_Y / 2);
-    snakeHead.dirX = DirectionX::LEFT;
-    snakeHead.dirY = DirectionY::NONE;
-    snake.push_front(snakeHead);
-
-    SnakeSegment *prevSegment = &snakeHead;
-    for (int i = 1; i < SNAKE_INIT_SIZE; ++i) {
-        SnakeSegment newSegment(prevSegment->x + 1, prevSegment->y);
-        newSegment.dirX = DirectionX::LEFT;
-        newSegment.dirY = DirectionY::NONE;
-        snake.push_back(newSegment);
-
-        prevSegment = &newSegment;
-    }
-
-    return snake;
-}
-
-void init(GameFieldArray &field, Snake &snake)
-{
-    snake = initSnake();
-
-    initField(field, snake);
-
-    initscr();                   // –ü–µ—Ä–µ—Ö–æ–¥ –≤ curses-—Ä–µ–∂–∏–º
-    keypad(stdscr, true);   //–í–∫–ª—é—á–∞–µ–º —Ä–µ–∂–∏–º —á—Ç–µ–Ω–∏—è —Ñ—É–Ω–∫—Ü–∏–æ–Ω–∞–ª—å–Ω—ã—Ö –∫–ª–∞–≤–∏—à
-
-    noecho();
-    halfdelay(5);
-
-    curs_set(0);
-}
-
-bool fieldCrash(const GameFieldArray &field, const SnakeSegment &head)
-{
-    if (field[head.y][head.x] == '*')
-        return true;
-    return false;
-}
-
-bool selfCrash(const Snake &snake, const SnakeSegment &head)
-{
-    for (auto &seg : snake) {
-        if (head == seg)
-            return true;
-    }
-
-    return false;
+	field[apple.y][apple.x] = '@';
 }
 
 bool appleFound(GameFieldArray &field, const SnakeSegment &head)
 {
-    if (field[head.y][head.x] == '@')
-    {
-        field[head.y][head.x] = ' ';
-        return true;
-    }
-    return false;
+	if (field[head.y][head.x] == '@')
+	{
+		field[head.y][head.x] = ' ';
+		return true;
+	}
+	return false;
 }
+
+bool fieldCrash(GameFieldArray &field, SnakeSegment &head)
+{
+	if (field[head.y][head.x] == '*')
+		return true;
+	return false;
+}
+
+bool selfCrash(const Snake &snake, const SnakeSegment &head)
+{
+	for (auto &seg : snake) {
+		if (head == seg)
+			return true;
+	}
+
+	return false;
+}
+
 
 bool moveSnake(GameFieldArray &field, Snake &snake)
 {
-    auto &back = snake.back();
-    snake.pop_back();
+	auto &back = snake.back();
+	snake.pop_back();
 
-    auto &head = snake.front();
-    auto nextMove = getNextMove(head);
+	auto &head = snake.front();
+	auto nextMove = getNextMove(head);
 
-    if (fieldCrash(field, nextMove) || selfCrash(snake, nextMove))
-        return true;
+	if (fieldCrash(field, nextMove) || selfCrash(snake, nextMove))
+		return true;
 
-    if (appleFound(field, nextMove))
-    {
-        snake.push_back(back);
-        addApple(field, snake);
-    }
+	if (appleFound(field, nextMove)) {
+		snake.push_back(back);
+		addApple(field, snake);
+	}
 
-    snake.push_front(nextMove);
+	snake.push_front(nextMove);
 
-    return false;
+	return false;
 }
 
 SnakeSegment getNextMove(SnakeSegment &head)
 {
-    unsigned int x = head.x + static_cast<unsigned int>(head.dirX);
-    unsigned int y = head.y + static_cast<unsigned int>(head.dirY);
-    return SnakeSegment(x, y, head.dirX, head.dirY);
+	unsigned int x = head.x + static_cast<unsigned int>(head.dirX);
+	unsigned int y = head.y + static_cast<unsigned int>(head.dirY);
+	return SnakeSegment(x, y, head.dirX, head.dirY);
+}
+
+Snake initSnake()
+{
+	Snake snake;
+
+	SnakeSegment snakeHead((FIELD_SIZE_X - SNAKE_INIT_SIZE) / 2, FIELD_SIZE_Y / 2, DirectionX::LEFT, DirectionY::NONE);
+	snake.push_front(snakeHead);
+
+	SnakeSegment *prevSegment = &snakeHead;
+	for (int i = 1; i < SNAKE_INIT_SIZE; ++i) {
+		SnakeSegment newSegment(prevSegment->x + 1, prevSegment->y, DirectionX::LEFT, DirectionY::NONE);
+		snake.push_back(newSegment);
+
+		prevSegment = &newSegment;
+	}
+
+	return snake;
+}
+
+void drawField(GameFieldArray &field, Snake &snake)
+{
+	// Draw field itself
+	for (GameFieldArray::size_type i = 0; i < FIELD_SIZE_Y; ++i) {
+		const char* fieldStr = field[i].data();
+		mvprintw(static_cast<int>(i), 0, fieldStr);
+	}
+
+	//Draw snake
+	for (auto &snakeSeg : snake) {
+		mvaddch(snakeSeg.y, snakeSeg.x, '*');
+	}
+}
+
+void init(GameFieldArray &field, Snake &snake)
+{
+	snake = initSnake();
+	initField(field, snake);
+
+	initscr();              // œÂÂıÓ‰ ‚ curses-ÂÊËÏ
+	keypad(stdscr, true);   //¬ÍÎ˛˜‡ÂÏ ÂÊËÏ ˜ÚÂÌËˇ ÙÛÌÍˆËÓÌ‡Î¸Ì˚ı ÍÎ‡‚Ë¯
+
+	noecho();
+	halfdelay(5);
+
+	curs_set(0);
+}
+
+void initField(GameFieldArray &field, Snake &snake)
+{
+	for (GameFieldArray::size_type i = 0; i < FIELD_SIZE_Y; ++i) {
+		field[i][FIELD_SIZE_X - 1] = '\0';
+	}
+
+	for (GameFieldArray::size_type i = 0; i < FIELD_SIZE_X - 1; ++i) {
+		field[0][i] = '*';
+		field[FIELD_SIZE_Y - 1][i] = '*';
+	}
+
+	for (GameFieldArray::size_type i = 1; i < FIELD_SIZE_Y - 1; ++i) {
+		field[i][0] = '*';
+		field[i][FIELD_SIZE_X - 2] = '*';
+
+		for (GameFieldArray::size_type j = 1; j < FIELD_SIZE_X - 2; ++j) {
+			field[i][j] = ' ';
+		}
+	}
+
+	addApple(field, snake);
 }
--- snake.h
+++ snake.h
@@ -1,36 +1,34 @@
 #pragma once
-#include <array>
-#include <list>
 
 enum class DirectionY {
-    UP = -1,
-    NONE = 0,
-    DOWN = 1,
+	UP = -1,
+	NONE = 0,
+	DOWN = 1,
 };
 
 enum class DirectionX {
-    LEFT = -1,
-    NONE = 0,
-    RIGHT = 1
+	LEFT = -1,
+	NONE = 0,
+	RIGHT = 1
 };
-
 
 class Point {
 public:
-    unsigned int x;
-    unsigned int y;
+	unsigned int x;
+	unsigned int y;
 
-    Point() : x(0), y(0) {}
-    Point(unsigned int x, unsigned int y) : x(x), y(y) {}
+	Point() : x(0), y(0) {}
+	Point(unsigned int x, unsigned int y) : x(x), y(y) {}
 };
 
 class SnakeSegment : public Point {
 public:
-    SnakeSegment(unsigned int _x, unsigned int _y) : Point(_x, _y), dirX(DirectionX::NONE), dirY(DirectionY::NONE) {}
-    SnakeSegment(unsigned int _x, unsigned int _y, DirectionX _dirX, DirectionY _dirY) : Point(_x, _y), dirX(_dirX), dirY(_dirY) {}
-    DirectionX dirX;
-    DirectionY dirY;
+	SnakeSegment(unsigned int _x, unsigned int _y) : Point(x, y), dirX(DirectionX::NONE), dirY(DirectionY::NONE) {}
+	SnakeSegment(unsigned int _x, unsigned int _y, DirectionX _dirX, DirectionY _dirY) : Point(_x, _y), dirX(_dirX), dirY(_dirY) {}
 
-    bool operator== (const SnakeSegment &segm) const { return x == segm.x && y == segm.y; }
-    bool operator== (const Point &point) const { return x == point.x && y == point.y; }
+	DirectionX dirX;
+	DirectionY dirY;
+
+	bool operator== (const Point &point) const { return x == point.x && y == point.y; }
+	bool operator== (const SnakeSegment &segm) const { return x == segm.x && y == segm.y; }
 };
